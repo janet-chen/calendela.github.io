06588048740bcd4067c9b124920bdff878663f3d
Anna Tran <anna@Annas-MacBook-Pro.local>
2019-06-09 13:37:48 -0700

    Add start page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NextButton from './components/NextButton';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick = buttonName => {
+    return NextButton;
+  };
+
+  render() {
+    return (
+      <div className="component-app">
+        <Start />
+      </div>
+    );
+  }
 }
 
 export default App;
